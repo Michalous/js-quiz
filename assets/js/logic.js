5f57d6845a374ac925cea7a48bf40931a8c938f4
Michalous <49964028+Michalous@users.noreply.github.com>
2023-01-10 20:48:20 +0000

    countdown added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,8 @@
 var submit_btn = document.getElementById('submit')
 var time_span = document.getElementById('time')
 
+var countDown = 120
+var setCountDown = setInterval(function() {
+    countDown -= 1
+    time_span.innerHTML = countDown
+}, 1000)
